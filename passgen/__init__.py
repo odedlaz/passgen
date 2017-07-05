e1e9bf7819a7cdfb7a80cf8c553dbb838707cd3f
Oded Lazar <odedlaz@gmail.com>
2017-07-05 18:18:47 +0300

    move assets to different dir

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,6 +1,11 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
 
+static_dir = os.path.abspath(os.path.join(app.root_path,
+                                          os.pardir,
+                                          'static'))
 from . import api
--- passgen/api.py
+++ passgen/api.py
@@ -1,15 +1,11 @@
-import os
 from flask import render_template, send_from_directory
 
-from passgen import app
-
-dirname = os.path.dirname(os.path.realpath(__file__))
-assets_dir = os.path.join(dirname, "assets")
+from passgen import app, static_dir
 
 
 @app.route('/assets/<path:path>')
 def send_assets(path):
-    return send_from_directory(assets_dir, path)
+    return send_from_directory(static_dir, path)
 
 
 @app.route('/', methods=['GET'])
