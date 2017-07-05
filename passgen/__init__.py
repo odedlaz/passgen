452d27eb00bdae53d95922ad38b03b862e517c3e
Oded Lazar <odedlaz@gmail.com>
2017-07-05 18:35:16 +0300

    move templates dir

--- passgen/__init__.py
+++ passgen/__init__.py
@@ -1,11 +1,14 @@
-import os
-
 from flask import Flask
+from os import pardir
+from os.path import abspath, dirname, join
 
-app = Flask(__name__)
+base_dir = abspath(join(dirname(__file__), pardir))
+
+static_dir = join(base_dir, "static")
+templates_dir = join(base_dir, "templates")
+
+app = Flask(__name__, template_folder=templates_dir)
 app.secret_key = 'why would I tell you my secret key?'
 
-static_dir = os.path.abspath(os.path.join(app.root_path,
-                                          os.pardir,
-                                          'static'))
+
 from . import api
